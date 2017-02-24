ea6ae38e0d04979f196712c881754fda1ef57727
jonschul <jonasschulze@mobile-232-190.wlan.uni-mannheim.de>
2017-02-24 13:17:20 +0100

    Aufgabenstellung

---
+++ MiniFloatAdder.java
@@ -0,0 +1,19 @@
+
+public class MiniFloatAdder {
+	/** prints all MiniFloat values in decreasing order */
+	public static void printAllMiniFloatValues() {
+        // YOUR_CODE_HERE
+	}
+
+	/** MiniFloat to double */
+	public static final double toDouble(byte miniFloat) {
+        // YOUR_CODE_HERE
+		return 0;
+	}
+
+	/** adds two non-negative MiniFloats */
+	public static final byte addMiniFloats(byte miniFloat1, byte miniFloat2) {
+        // YOUR_CODE_HERE
+		return 0;
+	}	
+}
